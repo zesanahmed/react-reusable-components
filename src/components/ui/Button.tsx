2fc19b49b58f5caa86113defd664c60f947bb8c6
zesanahmed <zesanahmed593@gmail.com>
2025-05-11 12:28:36 +0600

    Resolve errors

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,19 +1,39 @@
+import { ButtonHTMLAttributes, DetailedHTMLProps, forwardRef } from "react";
 import cn from "../../utils/cn";
 
-const Button = ({ className, variant }) => {
-  const getVariantClass = (variant) => {
-    switch (variant) {
-      case "outline":
-        return "btn-outline";
-      case "ghost":
-        return "btn-ghost";
-      default:
-        return "btn-solid";
-    }
-  };
-  return (
-    <button className={cn(getVariantClass(variant), className)}>Button</button>
-  );
+type Tref = HTMLButtonElement;
+type TVariant = "solid" | "outline" | "ghost";
+type TButton = {
+  variant?: TVariant;
 };
+type TButtonProps = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> &
+  TButton;
+
+const Button = forwardRef<Tref, TButtonProps>(
+  ({ className, variant = "solid", ...rest }, ref) => {
+    const getVariantClass = (variant: TVariant) => {
+      switch (variant) {
+        case "outline":
+          return "btn-outline";
+        case "ghost":
+          return "btn-ghost";
+        default:
+          return "btn-solid";
+      }
+    };
+    return (
+      <button
+        {...rest}
+        ref={ref}
+        className={cn(getVariantClass(variant), className)}
+      >
+        Button
+      </button>
+    );
+  }
+);
 
 export default Button;
