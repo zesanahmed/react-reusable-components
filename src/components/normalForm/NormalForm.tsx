510bcbc0e4165d1368fedafb3b1f87cb7a653cb2
zesanahmed <zesanahmed593@gmail.com>
2025-05-14 16:22:48 +0600

    Form element design

--- src/components/normalForm/NormalForm.tsx
+++ src/components/normalForm/NormalForm.tsx
@@ -1,4 +1,5 @@
 import { useForm } from "react-hook-form";
+import cn from "../../utils/cn";
 
 const NormalForm = () => {
   const { register, handleSubmit } = useForm();
@@ -6,26 +7,30 @@ const NormalForm = () => {
   const onSubmit = (data) => {
     console.log(data);
   };
+
+  const double = true;
   return (
     <div>
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="border-2 border-purple-500 max-w-5xl mx-auto
-        p-4"
+        className={cn("border-2 border-purple-500 max-w-lg mx-auto m-2 p-4", {
+          "max-w-5xl": double,
+          "max-w-md": !double,
+        })}
       >
-        <div className="grid grid-cols-2 gap-4">
-          <div>
+        <div
+          className={cn("grid grid-cols-1 justify-items-center gap-4", {
+            "grid-cols-2": double,
+            "grid-cols-1": !double,
+          })}
+        >
+          <div className="w-full max-w-md">
             <label htmlFor="name" className="block">
               Name
             </label>
-            <input
-              className="w-full"
-              type="text"
-              id="name"
-              {...register("name")}
-            />
+            <input className="" type="text" id="name" {...register("name")} />
           </div>
-          <div>
+          <div className="w-full max-w-md">
             <label htmlFor="email" className="block">
               Email
             </label>
@@ -36,15 +41,41 @@ const NormalForm = () => {
               {...register("email")}
             />
           </div>
-          <div>
+          <div className="w-full max-w-md">
             <label htmlFor="password" className="block">
               Password
             </label>
             <input
               className="w-full"
-              type="text"
+              type="password"
               id="password"
               {...register("password")}
+            />
+          </div>
+          <div className="w-full max-w-md">
+            <label htmlFor="password" className="block">
+              Select
+            </label>
+            <select className="rounded-md" {...register("select")}>
+              <option>one</option>
+              <option>two</option>
+              <option>three</option>
+              <option>four</option>
+            </select>
+          </div>
+          <div className="w-full max-w-md">
+            <label htmlFor="password" className="block">
+              Blog
+            </label>
+            <textarea className="appearance-none rounded-md"></textarea>
+          </div>
+          <div className="w-full max-w-md">
+            <label htmlFor="password" className="block">
+              Blog
+            </label>
+            <input
+              className="w-4 h-4 border-2 checked:bg-purple-500"
+              type="checkbox"
             />
           </div>
         </div>
